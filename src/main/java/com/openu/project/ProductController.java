02323f4613f00018f4634c88eae0b8c7b8dce6cf
yamit <yamitush13@gmail.com>
2021-03-03 17:20:43 +0200

    work in progress

--- src/main/java/com/openu/project/ProductController.java
+++ src/main/java/com/openu/project/ProductController.java
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/products")
-    public ArrayList<Product> product(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public ArrayList<Product> product(@RequestParam(value = "name", defaultValue = "orange") String name) {
         ArrayList<Product> list = new ArrayList<Product>();
-        list.add(new Product(1,"aa"));
-        list.add(new Product(2,"bb"));
-        list.add(new Product(3,"cc"));
-
-        //return new Product(counter.incrementAndGet(), String.format(template, name));
+        if(name != ""){
+            list.add(new Product(4,"orange"));
+        }else{
+            list.add(new Product(1,"aa"));
+            list.add(new Product(2,"bb"));
+            list.add(new Product(3,"cc"));
+        }
         return list;
     }
 }
--- src/main/java/com/openu/project/TimeController.java
+++ src/main/java/com/openu/project/TimeController.java
@@ -1,4 +1,4 @@
-package com.openu.project.controller;
+package com.openu.project;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
