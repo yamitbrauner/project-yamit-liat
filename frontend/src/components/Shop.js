c5df0fb0bbc06db8e572de1d912f2c32629ea3e4
yamit <yamitush13@gmail.com>
2021-03-14 12:02:18 +0200

    work in progress - frontend

--- frontend/src/components/Shop.js
+++ frontend/src/components/Shop.js
@@ -3,17 +3,18 @@ import SideMenu from './SideMenu';
 import Item from './Item';
 
 class Shop extends Component {
-  state = { items:[]};
-
-/*  constructor(props) {
+  state = { items:[],itemsFiltered:[], itemsToDisplay:[], categories: {}, categorySelected:2};
+  constructor(props) {
       super(props);
+      this.filterByCategory = this.filterByCategory.bind(this);
   }
-*/
+
   componentDidMount(){
-      this.setState({ items: [
+      this.setState({
+          items: [
               {
                   prod_id : 1,
-                  category_id:2,
+                  category_id:7,
                   prod_name:"קוראסון",
                   quantity_ordered : 10,
                   quantity_in_stock : 20,
@@ -22,7 +23,7 @@ class Shop extends Component {
                   pic_url : "https://www.foodisgood.co.il/wp-content/uploads/2015/11/rugelach.jpg"
               },{
                   prod_id : 1,
-                  category_id:2,
+                  category_id:3,
                   prod_name:"עוגיית שוקולד צ'יפס",
                   quantity_ordered : 10,
                   quantity_in_stock : 20,
@@ -48,10 +49,39 @@ class Shop extends Component {
                   description : "עוגת גבינה ופירות יער",
                   pic_url : "https://medias.hashulchan.co.il/www/uploads/2015/11/500_65061-600x600.jpg"
               },
-          ]})
-   }
+          ],
+          categories: {
 
+                  2: {
+                      category_name: "עוגות"
+                  },3:{
+                      category_name:"עוגיות"
+                  },4:{
+                      category_name:"ללא גלוטן"
+                  },5:{
+                      category_name:"פאי"
+                  },6:{
+                      category_name:"טבעוני"
+                  },7:{
+                      category_name:"שמרים"
+                  }
 
+              },
+      },()=>this.handleCategorySelection(2));
+  }
+    filterByCategory = (category) =>{
+        var tempItemsFiltered = [];
+        this.state.items.map(item=>{
+            if(item.category_id == category){
+                tempItemsFiltered.push(item);
+            }
+        })
+        this.setState({itemsFiltered:tempItemsFiltered, itemsToDisplay:tempItemsFiltered})
+    }
+    handleCategorySelection = (category) =>{
+        this.filterByCategory(category);
+        this.setState({categorySelected : category})
+    }
 
 
   render() {
@@ -60,11 +90,15 @@ class Shop extends Component {
             <div className="row">
                 <div className="col">
                     <div className="row">
-                        <SideMenu/>
+                        <SideMenu onSelectCategory={this.handleCategorySelection} categories={this.state.categories}/>
                     </div>
                 </div>
                 <div className="col-6">
-                    {this.state.items.map(item =>{
+                    <div className="row">
+                        <span className="col title">
+                            {this.state.categories[this.state.categorySelected] ? this.state.categories[this.state.categorySelected].category_name: "כללי"}</span>
+                    </div>
+                    {this.state.itemsToDisplay.map(item =>{
                         return <div className="row margin-top-bottom">
                             <Item item={item}/>
                         </div>
--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -1,49 +1,26 @@
 import React, { Component } from 'react';
 
 class SideMenu extends Component {
-  state = { categories:[]};
 
-  constructor(props) {
-      super(props);
-  }
-
-    componentDidMount(){
-        this.setState({ categories: [
-                {
-                    category_id : 1,
-                    category_name:"general"
-                },{
-                    category_id : 2,
-                    category_name:"Cakes"
-                },{
-                    category_id : 3,
-                    category_name:"Cookies"
-                },{
-                    category_id : 4,
-                    category_name:"Gluten_free"
-                },{
-                    category_id : 5,
-                    category_name:"Quiches_and_Pies"
-                },{
-                    category_id : 6,
-                    category_name:"Vegan"
-                },{
-                    category_id : 7,
-                    category_name:"Yeast"
-                },
-            ]})
+    constructor(props) {
+        super(props);
+        this.selectCategory = this.selectCategory.bind(this);
     }
+
+    selectCategory = (category) => {
+        this.props.onSelectCategory(category);
+    };
 
   render() {
     return (
         <div className="side-menu col">
-            {this.state.categories.map(category =>{
+            {Object.keys(this.props.categories).map((category,i) =>{
                 return <>
-                    <div className="row margin-top-bottom">
-                        <button className="btn">
-                            {category.category_name}
+                    <a key={i} className="row margin-top-bottom" onClick={()=>this.selectCategory(category)}>
+                        <button className="btn col category-name">
+                            {this.props.categories[category].category_name}
                         </button>
-                    </div>
+                    </a>
                     <div className="divider row margin-top-bottom"/>
                 </>
             })}
