d115e82d3e14e1f29cfe2a6f2e4998d653c8763f
yamit <yamitush13@gmail.com>
2021-03-21 12:49:09 +0200

    work in progress - frontend

--- frontend/src/components/Payment.js
+++ frontend/src/components/Payment.js
@@ -7,100 +7,43 @@ class Payment extends Component {
   render() {
     return (
         <div className="col payment">
-            <>
-                <div className="row">
-                    <span className="col">מועד אספקה</span>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <div className="row">
-                            <label>בחירת תאריך</label>
-                        </div>
-                        <div className="row">
-                            <input type="date"/>
-                        </div>
+            <form className="margin-top-bottom">
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="date">תאריך הזמנה</label>
+                        <input type="date" className="form-control" id="date" placeholder="תאריך"/>
                     </div>
                 </div>
-                <div className="divider row margin-top-bottom"/>
-            </>
-            <>
-                <div className="row">
-                    <span className="col">פרטים אישיים</span>
-                </div>
-                <div className="row">
-                    <div className="col form-group">
-                        <div className="row">
-                            <label>שם</label>
-                        </div>
-                        <div className="row">
-                            <input type="text" className="form-control"/>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="row">
-                            <label>טלפון</label>
-                        </div>
-                        <div className="row">
-                            <input type="text"/>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="row">
-                            <label>דוא"ל</label>
-                        </div>
-                        <div className="row">
-                            <input type="text"/>
-                        </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="name">שם מלא</label>
+                        <input type="text" className="form-control" id="name" placeholder="אנא הזן שם מלא"/>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col">
-                        <div className="row">
-                            <label>רחוב</label>
-                        </div>
-                        <div className="row">
-                            <input type="text"/>
-                        </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="address">כתובת</label>
+                        <input type="text" className="form-control" id="address" placeholder="אנא הזן כתובת"/>
                     </div>
-                    <div className="col">
-                        <div className="row">
-                            <label>מספר</label>
-                        </div>
-                        <div className="row">
-                            <input type="text"/>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="row">
-                            <label>עיר</label>
-                        </div>
-                        <div className="row">
-                            <input type="text"/>
-                        </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="city">עיר</label>
+                        <input type="text" className="form-control" id="city" placeholder="אנא הזן עיר"/>
                     </div>
                 </div>
-            </>
-            <div className="divider row margin-top-bottom"/>
-            <>
-                <div className="row">
-                    <span className="col">מידע נוסף</span>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <div className="row">
-                            <label>הערות</label>
-                        </div>
-                        <div className="row">
-                            <textarea class="col"/>
-                        </div>
+                <div className="divider"/>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="credit-card">מספר כרטיס אשראי</label>
+                        <input type="text" className="form-control" id="credit-card" placeholder="אנא הזמן מספר כרטיס אשראי"/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="id">מספר תז</label>
+                        <input type="text" className="form-control" id="id" placeholder="אנא הזן מספר תעודת זהות"/>
                     </div>
                 </div>
-            </>
-            <div className="divider row margin-top-bottom"/>
+                <button className="btn btn-primary">המשך לתשלום</button>
+            </form>
         </div>
-
-
-
     );
   }
 }
