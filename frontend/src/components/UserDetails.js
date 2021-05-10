2f440868808d3830cdd1468d27a5bcbfad5ae14b
yamit <yamitush13@gmail.com>
2021-05-10 16:44:34 +0300

    frontend - work in progress

--- frontend/src/components/UserDetails.js
+++ frontend/src/components/UserDetails.js
@@ -8,68 +8,74 @@ const disablePastDt = current => {
 };
 
 class UserDetails extends Component {
-    state = {isFinish: false, deliveryDate:null}
+    state = {isFinish: false, deliveryDate: null, isError:false};
 
     finishOrder = ()=>{
-        this.props.finishOrder(this.state.deliveryDate)
-        this.setState({isFinish: !this.state.isFinish})
+        if(this.state.deliveryDate === null){
+            this.setState({isError:true});
+        }else{
+            this.props.finishOrder(this.state.deliveryDate)
+            this.setState({isFinish: !this.state.isFinish})
+        }
     }
     onDateChange = (event)=>{
-        this.setState({deliveryDate:event._d});
+        this.setState({deliveryDate:event._d, isError:false});
     }
 
-  render() {
-      return (
-        <div className="row">
-            {!this.state.isFinish ?
-                <div className="col margin-top-bottom">
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
-                            <label htmlFor="date">תאריך הזמנה</label>
-                            <DatePicker
-                                onChange={this.onDateChange}
-                                class="form-control"
-                                isValidDate={disablePastDt}
-                                value={this.state.deliveryDate}
-                                dateFormat="DD/MM/YYYY"
-                                timeFormat={false}
-                            />
-                        </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
-                            <label htmlFor="name">שם מלא</label>
-                            <input type="text" className="form-control" id="name" disabled={true} value={this.props.userDetails.firstName + " " + this.props.userDetails.lastName}/>
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label htmlFor="address">כתובת להזמנה</label>
-                            <input type="text" className="form-control" id="address" disabled={true} value={this.props.userDetails.address}/>
-                        </div>
-                    </div>
+    render() {
+        return (
+          <div className="row">
+              {!this.state.isFinish ?
+                  <div className="col margin-top-bottom">
+                      <div className="form-row">
+                          <div className="form-group col-md-6">
+                              <label htmlFor="date">תאריך הזמנה</label>
+                              <DatePicker
+                                  onChange={this.onDateChange}
+                                  class="form-control"
+                                  isValidDate={disablePastDt}
+                                  value={this.state.deliveryDate}
+                                  dateFormat="DD/MM/YYYY"
+                                  timeFormat={false}
+                              />
+                              {this.state.isError ? <span className="error-txt">יש למלא תאריך הזמנה</span> : ""}
+                          </div>
 
-                    <div className="divider"/>
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
-                            <label htmlFor="id">מספר טלפון</label>
-                            <input type="text" className="form-control" id="phone" disabled={true} value={this.props.userDetails.phone}/>
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label htmlFor="id">מייל</label>
-                            <input type="text" className="form-control" id="mail" disabled={true} value={this.props.userDetails.mail}/>
-                        </div>
-                    </div>
-                    <button onClick={()=>this.finishOrder()} className="btn btn-lg margin-top-bottom btn-danger">למעבר לתשלום באמצעות Paypal</button>
-                </div>
-                :
-                <div>
-                    ההזמנה הושלמה בהצלחה
-                </div>
+                      </div>
+                      <div className="form-row">
+                          <div className="form-group col-md-6">
+                              <label htmlFor="name">שם מלא</label>
+                              <input type="text" className="form-control" id="name" disabled={true} value={this.props.userDetails.firstName + " " + this.props.userDetails.lastName}/>
+                          </div>
+                          <div className="form-group col-md-6">
+                              <label htmlFor="address">כתובת להזמנה</label>
+                              <input type="text" className="form-control" id="address" disabled={true} value={this.props.userDetails.address}/>
+                          </div>
+                      </div>
 
-            }
+                      <div className="divider"/>
+                      <div className="form-row">
+                          <div className="form-group col-md-6">
+                              <label htmlFor="id">מספר טלפון</label>
+                              <input type="text" className="form-control" id="phone" disabled={true} value={this.props.userDetails.phone}/>
+                          </div>
+                          <div className="form-group col-md-6">
+                              <label htmlFor="id">מייל</label>
+                              <input type="text" className="form-control" id="mail" disabled={true} value={this.props.userDetails.mail}/>
+                          </div>
+                      </div>
+                      <button onClick={()=>this.finishOrder()} className="btn btn-lg margin-top-bottom btn-danger">למעבר לתשלום באמצעות Paypal</button>
+                  </div>
+                  :
+                  <div>
+                      ההזמנה הושלמה בהצלחה
+                  </div>
 
-        </div>
-    );
-  }
+              }
+
+          </div>
+      );
+    }
 }
 
 export default UserDetails;
