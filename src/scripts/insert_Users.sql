4e3cb178279ea986ead35ff34f8ad24c97a6e2e2
Liat Arama <liat.arama1@gmail.com>
2021-05-29 01:26:05 +0300

    Backend: Add sql scripts

--- src/scripts/insert_Users.sql
+++ src/scripts/insert_Users.sql
@@ -1,8 +1,8 @@
 insert into public.Users
-values  (1, 'ליאת', 'ארמה', '0524325336', 'רמת גן', 'liat.arama1@gmail.com', '$2a$10$2vg/JT/J9vBUNgLOGMaEHuS1yElAzIBVezUmfySWc89R1ocuhTPVm', 1234 ,1),
-		(2, 'ימית', 'שמואל', '0522222222', 'חולון', 'yamitush13@gmail.com', '$2a$10$SD0o.qXlqRoQLb21X4h3JONV1WqK7jtMmlxmj4cmeM4WPIYYMd2qq', '12312', 1),
-		(3, 'נוי', 'עמית', '0532233232', 'אשקלון', 'noyamit1212@walla.com', '$2a$10$SD0o.qXlqRoQLb21X4h3JOG5xOroYFMMzItdkS/Wfktz3Bwb.RHIu', '985632', 2),
-		(4, 'עמרי', 'חלפון', '0545525465', 'בת ים', 'omrihahaha@gmail.com', '$2a$10$SD0o.qXlqRoQLb21X4h3JOqYmfC.UakggORooKC2shDcL2or1Ts1W', '45654', 2),
-		(5, 'ליאם', 'בוזגלו', '0506784561', 'אילת', 'liambu524@walla.co.il', '$2a$10$2vg/JT/J9vBUNgLOGMaEHuqz7iBJVaKqJ1FSZi3wqWuZ8zyPSuPAe', '6589658', 2);
+values  (1, 'ליאת', 'ארמה', '0524325336', 'רמת גן', 'liat.arama1@gmail.com', '$2a$10$2vg/JT/J9vBUNgLOGMaEHuS1yElAzIBVezUmfySWc89R1ocuhTPVm', '95b40bae-fdcd-478b-9c0e-5255df3e9fe8' ,1),
+		(2, 'ימית', 'שמואל', '0522222222', 'חולון', 'yamitush13@gmail.com', '$2a$10$SD0o.qXlqRoQLb21X4h3JONV1WqK7jtMmlxmj4cmeM4WPIYYMd2qq', '3bb07e30-e984-4135-8cf9-88c782b7159f', 1),
+		(3, 'נוי', 'עמית', '0532233232', 'אשקלון', 'noyamit1212@walla.com', '$2a$10$SD0o.qXlqRoQLb21X4h3JOG5xOroYFMMzItdkS/Wfktz3Bwb.RHIu', 'b7186eb0-1731-4440-a6a8-55be05ee91ae', 2),
+		(4, 'עמרי', 'חלפון', '0545525465', 'בת ים', 'omrihahaha@gmail.com', '$2a$10$SD0o.qXlqRoQLb21X4h3JOqYmfC.UakggORooKC2shDcL2or1Ts1W', 'adf1b720-dabb-4f24-b0ac-9112fa1cf235', 2),
+		(5, 'ליאם', 'בוזגלו', '0506784561', 'אילת', 'liambu524@walla.co.il', '$2a$10$2vg/JT/J9vBUNgLOGMaEHuqz7iBJVaKqJ1FSZi3wqWuZ8zyPSuPAe', '3e1e6672-3d10-4621-93bb-06ed76fb5f14', 2);
 		
 		
